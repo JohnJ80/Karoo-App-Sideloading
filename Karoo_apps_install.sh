3a795ddb05233fde0338b8d0ef34be178c9c4ead
JohnJ80 <43970366+JohnJ80@users.noreply.github.com>
2018-10-14 15:50:32 -0500

    Add files via upload
    Further refinement of backup and restore shell scripts.  Main refinement is to deal with and rename files that have spaces in the names in the uploaded apk's from the Karoo.

--- Karoo_apps_install.sh
+++ Karoo_apps_install.sh
@@ -1,9 +1,21 @@
+printf "Starting installation of Karoo apps. \n\n"
+
+printf "Removing spaces from filenames. \n\n"
+  
+for karoo_file in ./Karoo/apps/*;
+do
+ echo "removing spaces from apk file:" $karoo_file
+ mv "$karoo_file" "${karoo_file// /_}";
+done
+
+printf "Installing Karoo apps.....\n\n\n"
+
 for file in ./Karoo/apps/*;
 	do 
 		./adb install -r $file;
 	done
 
-printf "\n\nAll Done installing Karoo apps.\n"
+printf "\n\n All Done installing Karoo apps.\n"
 
 ./adb push ./Karoo/Android/data/net.osmand.plus /sdcard/Android/data
 ./adb push ./Karoo/Android/data/de.komoot.android /sdcard/Android/data
