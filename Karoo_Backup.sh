08c4f9373d5673e324b715769b983693599bdfec
JohnJ80 <43970366+JohnJ80@users.noreply.github.com>
2018-10-16 10:07:31 -0500

    Add files via upload
    Now creates a backup director for apk files and moves prior backup to that directory.  Automatically removes spaces from names upon completing backup of apk files from Karoo.

--- Karoo_Backup.sh
+++ Karoo_Backup.sh
@@ -5,7 +5,24 @@ printf "*                                            *\n"
 printf "----------------------------------------------\n"
 printf "\n\n\n"
 
+printf "Setting up directory for backup.\n"
+
+if [mkdir ./Karoo/apps_backup]
+	then
+	printf "Created backup directory with no issues."
+else
+	printf "Using backup directory to backup current apps backup.\n"
+fi
+
+printf "Copying previous backup to backup directory.\n"
+
+rm -r ./Karoo/apps_backup
+cp -R -v ./Karoo/apps ./Karoo/apps_backup
+
 printf "Backup of apps backed up using ES File Explored installed on Karoo.\n\n"
+
+rm -r ./Karoo/apps
+mkdir ./Karoo/apps
 
 ./adb pull /sdcard/backups/apps ./Karoo
 
@@ -18,4 +35,23 @@ printf "\n\n Backing up fo Komoot Data.\n\n"
 ./adb pull /sdcard/Android/data/de.komoot.android ./Karoo/Android/data
 
 printf  "\n\nAll done backup up Karoo apps plus Osmand and Komoot data.\n"
+
+printf "Renaming backed up apps without spaces in their names for easy re-install.\n"
+
+printf "Starting Rename_files.sh \n"
+
+for karoo_file in ./Karoo/apps/*;
+do
+ echo "removing spaces from apk file:" $karoo_file
+ mv "$karoo_file" "${karoo_file// /_}";
+done
+
+printf "\nList of backed up apps.\n\n"
+
+ls ./Karoo/apps
+
+printf "\nList of backup up Osmand maps.\n"
+
+ls -al ./Karoo/Android/data/net.osmand.plus/files/*.obf
+
 
